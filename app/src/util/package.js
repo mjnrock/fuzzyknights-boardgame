9a581dfc9a5e63c87932b3cc2c12183ad3d214b6
mjnrock <mjnrock@umich.edu>
2019-02-20 11:13:00 -0500

    init

---
+++ app/src/util/package.js
@@ -0,0 +1,11 @@
+import Bitwise from "./Bitwise.js";
+import Dice from "./Dice.js";
+import { WeightedPool } from "./WeightedPool.js";
+import { LinkedList } from "./LinkedList.js";
+
+export default {
+	Bitwise,
+	Dice,
+	WeightedPool,
+	LinkedList
+};
